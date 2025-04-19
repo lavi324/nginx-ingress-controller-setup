bfe56c7e26f3388fc7ebbce15f0023a71f8a868d
lavi324 <lavialduby@gmail.com>
2025-04-19 10:12:53 +0000

    new

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "google" {
   project = "coherent-answer-457220-s3"
-  region  = "us-central1"  # Change this to your desired region
+  region  = "us-central1"
 }
 
 # Define the VPC network
@@ -9,73 +9,64 @@ resource "google_compute_network" "vpc_network" {
   auto_create_subnetworks = false
 }
 
-# Define the subnet within the VPC with the specified IP range
+# Define the subnet within the VPC with a valid private IP range
 resource "google_compute_subnetwork" "subnet" {
   name          = "my-subnet"
-  ip_cidr_range = "220.15.4.0/24"
+  ip_cidr_range = "10.0.0.0/24"
   network       = google_compute_network.vpc_network.self_link
-  region        = "us-central1"  # Change this to your desired region
+  region        = "us-central1"
 }
 
-# Define the Kubernetes cluster
+# Define the Kubernetes cluster (GKE)
 resource "google_container_cluster" "my_cluster" {
   name     = "my-cluster"
-  location = "us-central1"  # Change this to your desired region
+  location = "us-central1"
 
-  # Use the subnet created above
-  network   = google_compute_network.vpc_network.self_link
-  subnetwork = google_compute_subnetwork.subnet.self_link  # Use the subnet's self-link as subnetwork
+  network    = google_compute_network.vpc_network.self_link
+  subnetwork = google_compute_subnetwork.subnet.self_link
 
-  # Node pool configuration
   node_pool {
-    name       = "default"
-    initial_node_count = 2  # Specify the desired number of nodes
-
-    node_locations = ["us-central1-a"]  # Specify the zone here
+    name               = "default"
+    initial_node_count = 2
+    node_locations     = ["us-central1-a"]
 
     node_config {
-      machine_type = "e2-medium"  # Change this to your desired machine type
-      disk_size_gb = 50           # Adjust the disk size as needed (default is 100GB)
-    }
-
-    # Autoscaling configuration (should match existing state)
-    autoscaling {
-      location_policy      = "BALANCED"
-      max_node_count       = 2
-      min_node_count       = 0
-      total_max_node_count = 0
-      total_min_node_count = 0
+      machine_type = "e2-medium"
+      disk_size_gb = 50
     }
   }
 }
 
+# Helm provider configuration
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"  # Path to your kubeconfig file
+    config_path = "~/.kube/config"
   }
 }
 
-# Install Helm charts
+# Jenkins Helm chart
 resource "helm_release" "jenkins" {
-  name       = "jenkins"
-  repository = "jenkins"  # This refers to the repository name you added with `helm repo add jenkins https://charts.jenkins.io`
-  chart      = "jenkins/jenkins"
-  namespace  = "jenkins"
-  version    = "5.8.36"
-  timeout = 800
+  name             = "jenkins"
+  repository       = "https://charts.jenkins.io"
+  chart            = "jenkins"
+  namespace        = "jenkins"
+  version          = "5.8.36"
+  timeout          = 800
   create_namespace = true
 }
 
-resource "helm_release" "argo-cd" {
-  name       = "argo-cd"
-  repository = "argo"  # This refers to the repository name you added with `helm repo add argo https://argoproj.github.io/argo-helm`
-  chart      = "argo/argo-cd"
-  namespace  = "argo"
-  version    = "7.8.26"
-  timeout = 800
+# Argo CD Helm chart
+resource "helm_release" "argo_cd" {
+  name             = "argo-cd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argo"
+  version          = "7.8.26"
+  timeout          = 800
   create_namespace = true
 }
 
+# MongoDB Helm chart
 resource "helm_release" "mongodb" {
   name             = "mongodb"
   repository       = "https://charts.bitnami.com/bitnami"
